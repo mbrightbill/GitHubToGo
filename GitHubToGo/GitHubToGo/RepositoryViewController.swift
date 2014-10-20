1c8e58af89e891b389e34f1c09cc531f3760de19
mbrightbill <brightbill.mt@gmail.com>
2014-10-20 14:18:31 -0700

    Added new working playground file

---
+++ GitHubToGo/GitHubToGo/RepositoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryViewController.swift
+//  GitHubToGo
+//
+//  Created by Matthew Brightbill on 10/20/14.
+//  Copyright (c) 2014 Matthew Brightbill. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- Networking.playground/section-1.swift
+++ Networking.playground/section-1.swift
@@ -1,22 +1,6 @@
 // Playground - noun: a place where people can play
 
 import UIKit
-import Foundation
 import XCPlayground
 
-XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
-
-let url = NSURL(string: "http://www.codefellows.org")
-
-let data = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
-    
-    if let httpReponse = response as? NSHTTPURLResponse {
-        swith httpResponse.statusCode {
-            case 200...204:
-        }
-    }
-    
-    
-})
-
-dataTask.resume()
+XCPExecutionShouldContinueIndefinitely()
