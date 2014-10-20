ffef89aef813e63a39d2d6892384c35d5817f440
mbrightbill <brightbill.mt@gmail.com>
2014-10-20 13:31:28 -0700

    Initial commit before Day 1

---
+++ GitHubToGo/GitHubToGo/NetworkController.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkController.swift
+//  GitHubToGo
+//
+//  Created by Matthew Brightbill on 10/20/14.
+//  Copyright (c) 2014 Matthew Brightbill. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+   
+    func fetchRepositoriesWithSearch() {
+        
+    }
+    
+}
+
+
+
+
+
+
---
+++ GitHubToGo/GitHubToGo/SplitContainerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SplitContainerViewController.swift
+//  GitHubToGo
+//
+//  Created by Matthew Brightbill on 10/20/14.
+//  Copyright (c) 2014 Matthew Brightbill. All rights reserved.
+//
+
+import UIKit
+
+class SplitContainerViewController: UIViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let splitVC = self.childViewControllers[0] as UISplitViewController
+        splitVC.delegate = self
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
+        return true
+    }
+
+
+}
