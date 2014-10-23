169f5f71737da31a77483d96497c9ef829da1591
mbrightbill <brightbill.mt@gmail.com>
2014-10-22 23:48:24 -0700

    Added a little bit of progress on Day 3 assignments

---
+++ GitHubToGo/GitHubToGo/UserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserViewController.swift
+//  GitHubToGo
+//
+//  Created by Matthew Brightbill on 10/22/14.
+//  Copyright (c) 2014 Matthew Brightbill. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
