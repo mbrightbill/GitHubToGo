18ec71642ec6b1cb3ad65bd6d62ba8105fd8b8a7
mbrightbill <brightbill.mt@gmail.com>
2014-10-26 17:03:12 -0700

    Cleaned up web view controller a little

--- GitHubToGo/GitHubToGo/WebViewController.swift
+++ GitHubToGo/GitHubToGo/WebViewController.swift
@@ -31,11 +31,4 @@ class WebViewController: UIViewController {
             println(url!)
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
 }
